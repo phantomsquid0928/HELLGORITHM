d63975373fea78031903ef459cc08092c86ef000
phantomsquid0928 <65645289+phantomsquid0928@users.noreply.github.com>
2024-01-09 17:35:33 +0900

    [Silver V] Title: 숫자 카드, Time: 248 ms, Memory: 80144 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int arr[20000001];
+int main() {
+//	int n;
+//	set<int> cards;
+//	
+//	cin >> n;
+//	for (int i = 0;i < n;i++) {
+//		int temp;
+//		cin >> temp;
+//		cards.insert(temp);
+//	}
+//	int m;
+//	cin >> m;
+//	for (int i = 0 ;i < m; i++) {
+//		int temp;
+//		cin >> temp;
+//		cout << cards.count(temp) << " ";
+////		if (cards.count(temp) != 0) cout << 1 << " ";
+////		else cout << 0 << " "; 
+//	}
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	for (int i =0 ;i < n; i++) {
+		int temp;
+		cin >> temp;
+		arr[temp + 10000000] = 1;
+	}
+	int m;
+	cin >> m;
+	for (int i = 0;i < m; i++) {
+		int temp;
+		cin >>temp;
+		if (arr[temp + 10000000] == 1) {
+			cout << 1 << " ";
+		}
+		else {
+			cout << 0 << " ";
+		}
+	}
+}

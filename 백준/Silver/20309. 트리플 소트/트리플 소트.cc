d87ae07379a09fa758268545944f6e43c74f1352
phantomsquid0928 <65645289+phantomsquid0928@users.noreply.github.com>
2024-01-15 10:35:04 +0900

    [Silver III] Title: 트리플 소트, Time: 32 ms, Memory: 3192 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20309. 트리플 소트/트리플 소트.cc
+++ 백준/Silver/20309. 트리플 소트/트리플 소트.cc
@@ -11,26 +11,22 @@ int main() {
 	for (int i = 0; i < n; i++){ 
 		cin>>arr[i];
 	}
-	for (int i = 1; i < n -1; i++) {
-		for (int j = n - 2; j >= i; j--) {
-			if (arr[j - 1] > arr[j + 1]) {
-				int t = arr[j - 1];
-				arr[j - 1] = arr[j + 1];
-				arr[j + 1] = t;
+	for (int i = 0; i < n; i++) {
+		if (i % 2 == 0) {
+//			cout << i << endl;
+			if (arr[i] % 2 != 1) {
+				cout << "NO" << endl;
+				return 0;
 			}
 		}
-		if (arr[i - 1] != i) {
-			cout << "NO" << endl;
-			
-			return 0;
+		else {
+//			cout << i  << "ff" << endl;
+			if (arr[i] % 2 != 0) {
+				cout << "NO" << endl;
+				return 0;
+			}
 		}
 	}
-	if (arr[n - 1] == n && arr[n - 2] == n - 1) {
-		cout << "YES" << endl;	
-	}
-	else {
-		cout << "NO" << endl;
-	}
-
+	cout << "YES" << endl;
 	
 }

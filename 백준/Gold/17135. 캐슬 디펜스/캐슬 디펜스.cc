50580651fde75b74ee6c1e62e991001a55240fcd
phantomsquid0928 <65645289+phantomsquid0928@users.noreply.github.com>
2025-01-17 23:59:19 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 16 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.cc
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.cc
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-vector<array<int, 3>> targets;
-vector<array<int, 3>> hist;
+vector<array<int, 2>> targets;
+vector<array<int, 2>> hist;
 
 int main() {
 	int n, m, d;
@@ -14,7 +14,7 @@ int main() {
 			int in;
 			cin >> in;
 			if (in == 0) continue;
-			targets.push_back({i, j, 0});
+			targets.push_back({i, j});
 		}
 	}
 	
@@ -41,7 +41,8 @@ int main() {
 		int res = 0;
 		
 		for (int tt = 0; tt < n; tt++) {
-			set<int> s;
+			
+			vector<int> to_delete(targets.size(), 0);
 			for (auto &a : archer) {
 				int mindist = 1000000;
 				int idx = -1;
@@ -53,7 +54,7 @@ int main() {
 					auto t = targets[i];
 					int tx = t[0];
 					int ty = t[1];
-					if (t[2]) continue;
+//					if (t[2]) continue;
 					
 					int dist = abs(x - tx) + abs(y - ty);
 					
@@ -70,36 +71,42 @@ int main() {
 					}
 				}
 				
-				if (idx != -1) s.insert(idx);
+				if (idx != -1) to_delete[idx] = 1;
 			}
 			
 //			cout << "selected";
-			for (auto &r : s) {
-//				cout << targets[r][0] << " " << targets[r][1] << ",";
-				targets[r][2] = 1;
-				res++;
+		
+//			cout << "selected";
+			int count = 0;
+			int count2 = 0;
+			targets.erase(remove_if(targets.begin(), targets.end(), [&](array<int, 2> & t) {
+				if (to_delete[count]) {
+					count2++;
+//					cout << targets[count][0] << " " << targets[count][1] << "]";
+				}
+				return to_delete[count++];
 			}
+			), targets.end());
+			res += count2;
+//			cout << endl;
+	
 //			cout << endl;
 			
 //			for (auto &t : targets) {
-//				if (t[2]) continue;
+////				if (t[2]) continue;
 //				cout << t[0] << " " << t[1] << "]";
 //			}
-//			cout << endl;
+////			cout << endl;
 			
-			int ss = 0;
-			for (auto &t : targets) {
-				if (t[2]) {
-					ss++;
-					continue;
-				}
-				
-				t = {t[0] + 1, t[1], t[2]};
-				if (t[0] == n) {
-					t[2] = 1;
-				}
-			}
-			if (ss == targets.size()) break;
+//			for (auto &t : targets) {
+//				
+//				t[0]++;
+//				if (t[0] == n) {
+//					
+//				}
+//			}
+			targets.erase(remove_if(targets.begin(), targets.end(), [&](array<int, 2> &t){ t[0]++; return t[0] == n;
+			}), targets.end());
 		}
 		maxres = max(res, maxres);
 	}

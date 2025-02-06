13fc64c57e74eae327240edb63e4e351db9cdeef
phantomsquid0928 <65645289+phantomsquid0928@users.noreply.github.com>
2025-02-06 16:21:58 +0900

    [Gold V] Title: 동전 2, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -2,33 +2,35 @@
 
 using namespace std;
 
-int n, k;
-int v[101];
-int coin[10001];
-int dp[10001];
+vector<int> arr;
+
+int dp[10001]; //dp n = i원을 쓸때 n원을  만드는데 드는 동전의 최소. 
+
+
 int main() {
+	int n, k;
 	cin >> n >> k;
-	int dup = 0;
-	for (int i = 0;i < n; i++) {
-		int t;
-		cin >> t;
-		v[i] = t;
+	
+	for (int i = 0;i <n ;i++ ) {
+		int in;
+		cin >> in;
+		
+		arr.push_back(in);
 	}
 	
-	memset(dp, 0x3f, sizeof(dp));
+	memset(dp, 0x3f, sizeof dp);
+	
 	dp[0] = 0;
-	for (int i = 1;i <= k;i++) {
-		for (int j = 0; j < n; j++) {
-			int value = v[j];
-			if (i >= value) {
-				dp[i] = min(dp[i], dp[i - value] + 1);
-			}
-		}
+	
+	for (int i = 0;i < n; i++ ) {
+		for (int j = arr[i]; j <= k; j++) {
+			
+			dp[j] = min(dp[j], dp[j - arr[i]] + 1);
+			
+		} 
 	}
-    if (dp[k] == 0x3f3f3f3f) {
-        cout << "-1\n";
-        return 0;
-    }
-
-	cout << dp[k] << endl;
+	
+	if (dp[k] == 0x3f3f3f3f) cout << -1 << endl;
+	
+	else cout << dp[k] << endl;
 }

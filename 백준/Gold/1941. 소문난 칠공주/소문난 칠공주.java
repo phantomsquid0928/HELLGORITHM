d5b52c7220399d6c239aac1555983933bbf8dc0e
phantomsquid0928 <65645289+phantomsquid0928@users.noreply.github.com>
2025-02-11 17:45:07 +0900

    [Gold III] Title: 소문난 칠공주, Time: 452 ms, Memory: 232636 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -2,68 +2,46 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int cnt = 0;
-	static char[][] arr;
-	static int[][] arr2;
-
+	static char[][] arr = new char[5][5];
+    static int cnt = 0;
 	static int[] dx = { 0, 0, 1, -1 };
 	static int[] dy = { 1, -1, 0, 0 };
 
-	static boolean[] dfsV = new boolean[25];
-	static boolean[][] bfsV;
-	// static ArrayList<int[]> groups = new ArrayList<>();
-
-	public static void dfs(int idx, int start, int[] sel) {
-		if (idx == 7) {
-			int sCount = 0;
-			arr2 = new int[5][5];
-
-			for (int i = 0; i < 7; i++) {
-				int x = sel[i] / 5;
-				int y = sel[i] % 5;
-
-				arr2[x][y] = 1;
-
-				if (arr[x][y] == 'S') {
-					sCount += 1;
-				}
-			}
-
-			if (sCount >= 4) {
-				int x = sel[0] / 5;
-				int y = sel[0] % 5;
-				bfsV = new boolean[5][5];
-				bfs(x, y);
-
-				for (int i = 0; i < 7; i++) {
-					int x2 = sel[i] / 5;
-					int y2 = sel[i] % 5;
-
-					if (!bfsV[x2][y2]) {
-						return;
-					}
-				}
-				cnt += 1;
-				// groups.add(sel.clone());
-			}
-			return;
+	static void dfs(int d, int start, int sCount, int[] sel) {
+		if (d == 7) {
+			if (sCount < 4) return;
+            if (bfs(sel)) {
+                cnt++;
+            }
+            return;
 		}
 
 		for (int i = start; i < 25; i++) {
-			if (!dfsV[i]) {
-				dfsV[i] = true;
-				sel[idx] = i;
-				dfs(idx + 1, i + 1, sel);
-				dfsV[i] = false;
-			}
+            int x = i / 5, y = i % 5;
+            if (arr[x][y] == 'S') {
+                sel[d] = i;
+                dfs(d + 1, i + 1, sCount + 1, sel);
+            } else {
+                if (d - sCount + 1 >= 4) continue;
+                sel[d] = i;
+                dfs(d + 1, i + 1, sCount, sel);
+            }
 		}
 	}
 
-	public static void bfs(int x, int y) {
+	public static boolean bfs(int[] sel) {
+        boolean[][] arr2 = new boolean[5][5];
+        for (int s : sel) {
+            arr2[s/5][s%5] = true;
+        }
+        boolean[][] visited = new boolean[5][5];
 		Deque<int[]> queue = new ArrayDeque<>();
 
-		queue.add(new int[] { x, y });
-		bfsV[x][y] = true;
+        int x = sel[0] / 5, y = sel[0] % 5;
+        visited[x][y] = true;
+
+		queue.offer(new int[] { x, y });
+        int sevenCnt = 1;
 
 		while (!queue.isEmpty()) {
 			int[] cur = queue.poll();
@@ -72,12 +50,14 @@ public class Main {
 				int nx = cur[0] + dx[i];
 				int ny = cur[1] + dy[i];
 
-				if (nx >= 0 && ny >= 0 && nx < 5 && ny < 5 && !bfsV[nx][ny] && arr2[nx][ny] == 1) {
-					queue.add(new int[] { nx, ny });
-					bfsV[nx][ny] = true;
+				if (nx >= 0 && ny >= 0 && nx < 5 && ny < 5 && !visited[nx][ny] && arr2[nx][ny]) {
+					queue.offer(new int[] { nx, ny });
+					visited[nx][ny] = true;
+                    sevenCnt++;
 				}
 			}
 		}
+        return sevenCnt == 7;
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -88,15 +68,7 @@ public class Main {
 			arr[i] = br.readLine().toCharArray();
 		}
 
-		dfs(0, 0, new int[7]);
-
-		// System.out.println(Arrays.deepToString(arr));
-		// System.out.println(groups.size());
-
-		// for (int[] a : groups) {
-		// 	System.out.println(Arrays.toString(a));
-		// }
-
+		dfs(0, 0, 0, new int[7]);
 		System.out.println(cnt);
 	}
 }
